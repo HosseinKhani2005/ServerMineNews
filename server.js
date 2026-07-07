914eb793ca9ec3d32b88d1c1e4431917962bd74d
hossein <hkhani1384@gmail.com>
2026-07-07 03:43:31 +0330

    default

--- server.js
+++ server.js
@@ -13,29 +13,29 @@ const { authorizationUser } = require("./middleware/user.middleware");
 const app = express();
 const port = process.env.PORT || 2000;
 
-const corsOptions = {
-  origin: [
-    "http://localhost:3000",
-    "https://www.hosseinkhani20.ir",
-    "https://hosseinkhani20.ir",
-    "https://new-project-mu-ten.vercel.app",
-  ],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
-
-// اعمال CORS به همه مسیرها
-app.use(cors(corsOptions));
-
-// 2. مدیریت اختصاصی درخواست‌های OPTIONS (برای اطمینان بیشتر)
-app.options('*', cors(corsOptions));
-
+app.use(
+  helmet({
+    crossOriginResourcePolicy: { policy: "cross-origin" },    
+    crossOriginOpenerPolicy: { policy: "same-origin-allow-popups" } 
+  })
+);
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://www.hosseinkhani20.ir",
+      "https://new-project-mu-ten.vercel.app",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
-app.set("trust proxy", 1);
+app.set('trust proxy', 1); 
 app.use("/api/auth", apiRouter);
-app.use("/api/user", authorizationUser, userRouter);
+app.use("/api/user", authorizationUser ,userRouter);
 
 app.get("/", (req, res) => {
   res.status(200).json({ ok: true });
